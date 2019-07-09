ea81c9526dd9edbedbd091d11d3c2a581dfc2570
pkraker <pkraker@openknowledgemaps.org>
2019-07-09 12:13:11 +0200

    Removes href for beta banner. Fixes #33

--- components/vis_beta_banner.php
+++ components/vis_beta_banner.php
@@ -14,7 +14,7 @@
 
     <div class="alert alert-warning" id="desktop-warning">
 
-        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <a class="close" data-dismiss="alert">&times;</a>
 
         Welcome to the <span style="font-weight:bold;">beta version</span> of Open Knowledge Maps. For more information on this map, please 
         <a href="#info_modal" data-type="text" data-toggle="modal" class="underline" style="font-weight:bold;">click here</a>.
