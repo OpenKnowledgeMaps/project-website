4ced4d3cd04d72d71019a453a9ca31d483a59e4a
Christopher Kittel <web@christopherkittel.eu>
2019-10-30 13:24:04 +0100

    cookie banner corrected

--- components/head_privacy_graphvis.php
+++ components/head_privacy_graphvis.php
@@ -3,12 +3,12 @@
 <script>
 
 <?php if ($BROWSER_LANG === "de") { ?>
-        let cookie_message = 'Wir verwenden Cookies, um Concept Graph für Sie möglichst benutzerfreundlich zu gestalten. Wenn Sie fortfahren, nehmen wir an, dass Sie mit der Verwendung von Cookies auf dieser Webseite einverstanden sind. Weitere Informationen entnehmen Sie bitte ';
+        let cookie_message = 'Concept Graph fällt unter die gemeinsame Verantwortlichkeit von Open Knowledge Maps und Know Center. Es werden keine persönlich identifizierenden Daten erhoben. Im Zuge des Projekts werden anonymisiert Daten über Ihr Nutzungsverhalten (Clicks) gesammelt und Cookies verwendet, um Concept Graph für Sie möglichst benutzerfreundlich zu gestalten. Wenn Sie fortfahren, nehmen wir an, dass Sie mit der anonymen Datenerhebung und Verwendung von Cookies auf dieser Webseite einverstanden sind. Weitere Informationen entnehmen Sie bitte ';
         let cookie_link = "Concept Graph Privacy Link";
         let cookie_button = "Alles klar!";
         let cookie_href = "https://openknowledgemaps.org/datenschutz-conceptgraph";
 <?php } else { ?>
-        let cookie_message = 'We use cookies to improve your experience. By your continued use of Concept Graph you accept such use. For more information, please see ';
+        let cookie_message = 'Concept Graph falls under the Joint Controllership of Open Knowledge Maps and Know Center. We will not collect any personal identifying information. For this project anonymised data (clicks) will be collected and cookies used to improve your experience. By your continued use of Concept Graph you accept such use. For more information, please see ';
         let cookie_link = "Concept Graph Privacy Link";
         let cookie_button = "Got it!";
         let cookie_href = "https://openknowledgemaps.org/privacy-conceptgraph";
@@ -46,16 +46,16 @@
             }
         })
     });
-    
+
     function getCookie(name) {
         var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
         return v ? v[2] : null;
     }
-    
+
     function setCookie(name, value, days) {
         var d = new Date;
         d.setTime(d.getTime() + 24*60*60*1000*days);
         document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
     }
-    
+
 </script>
