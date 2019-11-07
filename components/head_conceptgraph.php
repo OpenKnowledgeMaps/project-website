f27b310c1d21476d2a352dcd479be45171400f13
Christopher Kittel <web@christopherkittel.eu>
2019-11-07 21:35:33 +0100

    applied conceptgraph naming convention

--- components/head_conceptgraph.php
+++ components/head_conceptgraph.php
@@ -1,5 +1,4 @@
 <?php include('head_basic.php') ?>
-<?php include('head_privacy_graphvis.php') ?>
+<?php include('head_privacy_conceptgraph.php') ?>
 <?php include('head_headstart.php') ?>
 <?php include('head_bootstrap.php') ?>
-
