91c38d2ff53963a1143a1b964c2fa0389bc564b5
Thomas Arrow <thomasarrow@gmail.com>
2019-11-15 15:43:18 +0000

    introduce simple error heuristics and qunit

---
+++ js/error_heuristics.js
@@ -0,0 +1,14 @@
+var getTooFewPapersHeuristic = function ( post_data ) {
+    if ( !post_data ) return 'unknown';
+    if( post_data.from && post_data.to ) {
+        var fromDate = Date.parse( post_data.from );
+        var toDate = Date.parse( post_data.to );
+        var millisecondsInSixtyDays = 1000*60*60*24*60;
+        if (toDate - fromDate <= millisecondsInSixtyDays) return 'shorttimedelta';
+    }
+    if( post_data.q ) {
+        var tokens = post_data.q.split(' ')
+        return tokens.length < 4 ? 'toofewtokens' : 'toomanytokens';
+    }
+    return 'unknown';
+}
---
+++ js/tests.js
@@ -0,0 +1,14 @@
+QUnit.test( "returns shorttimedelta error if timeframe is 60 or less days", function( assert ) {
+    var post_data  = { from: '2019-01-01', to: '2019-01-02' }
+    assert.strictEqual( getTooFewPapersHeuristic( post_data ), 'shorttimedelta' );
+  });
+
+  QUnit.test( "returns toomanytokens error if query string has 4 or more words", function( assert ) {
+    var post_data  = { q: 'the quick brown fox jumped over the lazy dog' }
+    assert.strictEqual( getTooFewPapersHeuristic( post_data ), 'toomanytokens' );
+  });
+
+  QUnit.test( "returns toofewtokens error if querystring has less than 4 words", function( assert ) {
+    var post_data  = { q: 'the quick brown' }
+    assert.strictEqual( getTooFewPapersHeuristic( post_data ), 'toofewtokens' );
+  });
