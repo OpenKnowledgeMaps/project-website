804931d0293cafecc49848cea613541cedf110ca
Thomas Arrow <thomasarrow@gmail.com>
2019-11-15 16:47:38 +0000

    integrate heuristic checker

--- components/error-modal.php
+++ components/error-modal.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="./css/error-modal.css">
 <script type="text/javascript" src="./js/search_string.js"></script>
 <script type="text/javascript" src="./js/error_heuristics.js"></script>
+<script type="text/javascript" src="./js/error_heuristics.js"></script>
 
 <!-- The Modal -->
 <div id="error_modal" class="modal">
@@ -22,11 +23,9 @@
 </div>
 
 <script type="text/javascript">
-  var q = localStorage.getItem("q");
-  var from = localStorage.getItem("q");
-  var to = localStorage.getItem("q");
-  var service = localStorage.getItem("q");
-  var error_type = localStorage.getItem("error_type");
+  var post_data = JSON.parse(sessionStorage.getItem("post_data"));
+  var status = sessionStorage.getItem("status");
+  var service = sessionStorage.getItem("service");
   // Get the modal
   var modal = document.getElementById("error_modal");
 
@@ -36,6 +35,11 @@
   // Get the <span> element that closes the modal
   var span = document.getElementById("error_modal_close");
 
+  var queryString = "";
+  if(post_data) {
+    queryString = post_data.q;
+  }
+
   let search_string = "";
   try {
       search_string = getServiceSearchString(post_data, service);
@@ -43,23 +47,22 @@
       console.log("An error ocurred when creating the search string");
   }
 
+  const error_type = getTooFewPapersHeuristic( post_data );
+
   $(function() {
     var modal = $('#error_modal');
-    modal.find('.error-modal-q').text(q);
+    modal.find('.error-modal-q').text(queryString);
     modal_btn.style.visibility="hidden";
-    if ( error_type == "typo") {
-      modal.find('.error-message-body p').text('Error type TYPO detected.');
+    if ( error_type === "toospecific") {
+      modal.find('.error-message-body p').text('Your search was too specific');
       modal_btn.style.visibility="visible";
-    }
-    if ( error_type == 2) {
-      modal.find('.error-message-body p').text('Error type 2 detected.');
+    } else if ( error_type === "toomanytokens") {
+      modal.find('.error-message-body p').text('Your search might be too long');
       modal_btn.style.visibility="visible";
-    }
-    if ( error_type == 3) {
-      modal.find('.error-message-body p').text('Error type 3 detected.');
+    } else if ( error_type == "shorttimedelta") {
+      modal.find('.error-message-body p').text('The timeframe is too small choose longer than 60 days');
       modal_btn.style.visibility="visible";
-    }
-    if ( error_type == 4) {
+    } else {
       modal.find('.error-message-body p').text('Error type 4 detected.');
       modal_btn.style.visibility="visible";
     $("#service-outlink").html("Sorry! We could not create a map for your search term. Most likely there were not enough results."
