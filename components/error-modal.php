41d8647a3250113c3abbc5173aff1997c4fa5d68
Christopher Kittel <web@christopherkittel.eu>
2019-11-15 16:48:59 +0100

    search string outlink

--- components/error-modal.php
+++ components/error-modal.php
@@ -12,6 +12,8 @@
     <div class="error-message-body">
       <p>Default error text</p>
     </div>
+    <div id="service-outlink" class="error-message-body">
+    </div>
     <span id="error_modal_close" class="close">Try again</span>
   </div>
 </div>
@@ -31,6 +33,12 @@
   // Get the <span> element that closes the modal
   var span = document.getElementById("error_modal_close");
 
+  let search_string = "";
+  try {
+      search_string = getServiceSearchString(post_data, service);
+  } catch(e) {
+      console.log("An error ocurred when creating the search string");
+  }
 
   $(function() {
     var modal = $('#error_modal');
@@ -51,6 +59,11 @@
     if ( error_type == 4) {
       modal.find('.error-message-body p').text('Error type 4 detected.');
       modal_btn.style.visibility="visible";
+    $("#service-outlink").html("Sorry! We could not create a map for your search term. Most likely there were not enough results."
+            + ((search_string !== "")
+                ?("<br> You can <a href=\"" + search_string + "\" target=\"_blank\">check out your search on " + ((service === "base") ? ("BASE") : ("PubMed")) + "</a> or <a href=\"index.php\">go back and try again.</a>")
+                :("<br> Please <a href=\"index.php\">go back and try again.</a>"))
+            + "<br><br>If you think that there is something wrong with our site, please let us know at <br><a href=\"mailto:info@openknowledgemaps.org\">info@openknowledgemaps.org</a>");
     }
   });
   // When the user clicks on the button, open the modal
