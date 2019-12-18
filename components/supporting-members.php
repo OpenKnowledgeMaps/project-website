682090cf7cd05e53af63d93b7e702c29e15a993d
tanteuschi <maxi@openknowledgemaps.org>
2019-12-18 15:32:29 +0100

    Supporting members alphabetical order

--- components/supporting-members.php
+++ components/supporting-members.php
@@ -1,4 +1,4 @@
-<a href="https://lbg.ac.at/" target="_blank"><img src="./img/supporting-members/bos/lbg-oisc.png" alt="Logo LBG"></a>
 <a href="https://www.akbild.ac.at/portal_en/akbild_startpage?set_language=en&cl=en" target="_blank"><img src="./img/supporting-members/bos/akbild_logo.png" alt="Logo AKademie der Bildenden Künste Wien"></a>
-<a href="https://www.zbw.eu/en/" target="_blank"><img src="./img/supporting-members/bos/logo-zbw.jpg" alt="Logo ZBW"></a>
 <a href="https://publications.copernicus.org/" target="_blank"><img src="./img/supporting-members/bos/Logo-Copernicus-Pub.png" alt="Logo Copernicus Publishing"></a>
+<a href="https://lbg.ac.at/" target="_blank"><img src="./img/supporting-members/bos/lbg-oisc.png" alt="Logo LBG"></a>
+<a href="https://www.zbw.eu/en/" target="_blank"><img src="./img/supporting-members/bos/logo-zbw.jpg" alt="Logo ZBW"></a>
