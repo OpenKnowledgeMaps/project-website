1364b904b3eefd218bbbfd6afc4e3503b2b32bcc
pkraker <pkraker@openknowledgemaps.org>
2020-02-27 20:41:52 +0100

    Fix default zika map

--- js/data-config_plos.js
+++ js/data-config_plos.js
@@ -10,7 +10,7 @@ var data_config = {
     show_multiples: false,
     show_dropdown: false,
     preview_type: "pdf",
-    sort_options: ["views", "title", "authors", "year"],
+    sort_options: ["readers", "title", "authors", "year"],
     is_force_areas: true,
     language: "eng_plos",
     area_force_alpha: 0.015,
