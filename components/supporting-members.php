aac44cd6ca35a1280ab5e7f84b974fed73c67fa7
pkraker <pkraker@openknowledgemaps.org>
2020-03-26 23:37:08 +0100

    Revert "Merge remote-tracking branch 'remotes/origin/supportingmember-univie'"
    This reverts commit 063737470dcdfc63384dfe6c307f171862b321f8, reversing
    changes made to f3dcf3d9ea37670cfa76a07ec9b3dae2110a95f2.

--- components/supporting-members.php
+++ components/supporting-members.php
@@ -1,5 +1,4 @@
-<a href="https://www.univie.ac.at/" target="_blank"><img src="./img/supporting-members/bos/univie-logo.png" alt="Logo University Vienna"></a>
+<a href="https://www.akbild.ac.at/portal_en/akbild_startpage?set_language=en&cl=en" target="_blank"><img src="./img/supporting-members/bos/akbild_logo.png" alt="Logo AKademie der Bildenden Künste Wien"></a>
 <a href="https://publications.copernicus.org/" target="_blank"><img src="./img/supporting-members/bos/Logo-Copernicus-Pub.png" alt="Logo Copernicus Publishing"></a>
 <a href="https://lbg.ac.at/" target="_blank"><img src="./img/partners/lbg-oisc.png" alt="Logo LBG"></a>
 <a href="https://www.zbw.eu/en/" target="_blank"><img src="./img/supporting-members/bos/logo-zbw.jpg" alt="Logo ZBW"></a>
-<a href="https://www.akbild.ac.at/portal_en/akbild_startpage?set_language=en&cl=en" target="_blank"><img src="./img/supporting-members/bos/akbild_logo.png" alt="Logo AKademie der Bildenden Künste Wien"></a>
