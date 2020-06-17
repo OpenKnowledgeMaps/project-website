d3f3e094cc9e12a79a73ae9664eea34e596f1fa0
tanteuschi <maxi@openknowledgemaps.org>
2020-06-17 15:30:37 +0200

    covis banner update including mobile version

--- components/covis_banner.php
+++ components/covis_banner.php
@@ -1,27 +1,26 @@
-<div class="covis-banner" style="background-color: #eff3f4;">
+<div class="covis-banner">
     <div class="mobile-donation-banner">
-        <p style="color:#1e6662; font-weight: 700; text-align: center;">Covis: Knowledge Map on Covid-19 research curated by subject matter experts</p>
+        <h3 class="banner-headline"><a target="_blank" class="underline" href="https://openknowledgemaps.org/covis/"><i class="fa fa-bell" style="color: #e65238; font-size: 20px;"></i> CoVis: A new tool to kick start your COVID-19 literature research</a></h3>
 
-        <p class="try-now" style="text-align:center; margin:20px 0 0;">
-            <a target="_blank" class="donate-now" href="">Check out CoVis</a>
-        </p>
+                <p style="padding-top:10px;">For the development of therapeutics and vaccines for COVID-19, scientists need reliable research results. To support them, Open Knowledge Maps and ReFigure have launched CoVis. The new tool provides a curated knowledge map of seminal works on COVID-19.
+                <a target="_blank" style="font-weight: 800;" class="underline" href="https://openknowledgemaps.org/covis/">Try out CoVis</a></p>
     </div>
 
     <div class="desktop-donation-banner" style="background-color: #eff3f4;">
         <div style="max-width:1200px; margin:0px auto 0;">
 
             <div class="donation-image">
-                <a  target="_blank" href="">
+                <a  target="_blank" href="https://openknowledgemaps.org/covis/">
                     <img src="./img/project-examples/covis-banner.png" alt='Check out CoVis a knowledge base for Covid-19 research curated by subject-matter experts'></a>
             </div>
 
             <div class="donation-text">
-                <h3 style="color: #1e6662;">CoVis a curated, visual knowledge base for Covid-19 research</h3>
+                <h3 class="banner-headline">CoVis: A new tool to kick start your COVID-19 literature research</h3>
 
-                <p>In the current pandemic, many researchers seek valid scientific data and findings on Covid-19. To support them, Open Knowledge Maps and ReFigure have launched CoVis. CoVis provides a curated knowledge map of seminal works on Covid-19 research.
+                <p>For the development of therapeutics and vaccines for COVID-19, scientists need reliable research results. To support them, Open Knowledge Maps and ReFigure have launched CoVis. The new tool provides a curated knowledge map of seminal works on COVID-19.
                 </p> 
                 <p class="try-now" style="text-align:left; margin:20px 0 0;">
-                    <a target="_blank" class="donate-now" href="">Try it out</a>
+                    <a target="_blank" class="donate-now" href="https://openknowledgemaps.org/covis/">Try out CoVis</a>
 
                 </p>
             </div>
