d78f06ef415868c7207ee49f7da0da911274c310
pkraker <pkraker@openknowledgemaps.org>
2020-06-19 21:24:18 +0200

    Make CoVis links relative

--- components/covis_banner.php
+++ components/covis_banner.php
@@ -1,6 +1,6 @@
 <div class="covis-banner">
     <div class="mobile-donation-banner">
-        <h3 class="banner-headline"><a target="_blank" class="underline" href="https://openknowledgemaps.org/covis/"><i class="fa fa-bell" style="color: #e65238; font-size: 20px;"></i> CoVis: A new tool to discover reliable COVID-19 research</a></h3>
+        <h3 class="banner-headline"><a target="_blank" class="underline" href="covis/"><i class="fa fa-bell" style="color: #e65238; font-size: 20px;"></i> CoVis: A new tool to discover reliable COVID-19 research</a></h3>
 
                 <p style="padding-top:10px;">For the development of therapeutics and vaccines for COVID-19, scientists depend on reliable research results. To support them, Open Knowledge Maps and ReFigure have launched CoVis: a curated &amp; constantly evolving knowledge map of seminal works on COVID-19.
                 <a target="_blank" style="font-weight: 800;" class="underline" href="https://openknowledgemaps.org/covis/">Try out CoVis</a></p>
@@ -10,7 +10,7 @@
         <div style="max-width:1200px; margin:0px auto 0;">
 
             <div class="donation-image">
-                <a  target="_blank" href="https://openknowledgemaps.org/covis/">
+                <a  target="_blank" href="covis/">
                     <img src="./img/project-examples/covis-teaser.png" alt='Check out CoVis a knowledge base for Covid-19 research curated by subject-matter experts'></a>
             </div>
 
@@ -21,7 +21,7 @@
                 </p> 
                 
                 <p class="try-now" style="text-align:left; margin:20px 0 0;">
-                    <a target="_blank" class="donate-now" href="https://openknowledgemaps.org/covis/">Try out CoVis</a>
+                    <a target="_blank" class="donate-now" href="covis/">Try out CoVis</a>
                 </p>
                 
             </div>
