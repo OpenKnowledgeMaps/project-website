2c6d2f8d87e2c10d76f4d07c02f38463c25533e8
pkraker <pkraker@openknowledgemaps.org>
2020-07-14 17:47:30 +0200

    Add HTML skeleton for error messages, more restructuring

--- search.php
+++ search.php
@@ -43,7 +43,30 @@ if(!empty($_POST)) {
         include($COMPONENTS_PATH . 'head_headstart.php');
         ?>
 
-
+        <script>
+            //Set JavaScript values influenced by PHP & error code translations
+            const error_code_translation =
+                    {
+                        
+                    }
+            
+            <?php
+                if(isset($post_data)) {
+                    echo "var post_data = " . $post_data . ";\n";
+                }
+                
+                if(isset($unique_id)) {
+                    echo 'var unique_id = "' . $unique_id . '";';
+                }
+                
+                $service_get = filter_input(INPUT_GET, "service", FILTER_SANITIZE_STRING);
+                $service = (isset($service_get)?($service_get):(""))
+                
+            ?>
+            
+            var service = "<?php echo $service ?>";
+            
+        </script>
 
         <style>
 
@@ -75,12 +98,23 @@ if(!empty($_POST)) {
              <?php include ($COMPONENTS_PATH . "browser_unsupported_banner.php"); ?>
 
             <div id="progress" class="mittig">
-                <h3 class="visualize">Your knowledge map on <span id="search_term"></span> is being created!</h3>
+                <div id="active_state" class="search_active_state">
 
-                <div id="progressbar"></div>
+                    <h3 class="visualize">Your knowledge map on <span id="search_term"></span> is being created!</h3>
 
-                <p id="status" class="animated-ellipsis">Please be patient, this takes about 20 seconds.
-                </p>
+                    <div id="progressbar"></div>
+
+                    <p id="status" class="animated-ellipsis">Please be patient, this takes about 20 seconds.
+                    </p>
+                </div>
+                
+                <div id="error_state" class="search_error_state nodisplay">
+                    <div id="error-title"></div>
+                    <div id="error-reason"></div>
+                    <div id="error-remedy"></div>
+                    <div id="error-more-info"></div>
+                    <div id="error-contact"></div>
+                </div>
 
             </div>
 
@@ -98,61 +132,7 @@ if(!empty($_POST)) {
         </div>
 
         <script>
-            //Everything related to the request
-            
-            function fallbackCheck() {
-                $.getJSON("<?php echo $HEADSTART_URL ?>server/services/getLastVersion.php?vis_id=" + unique_id,
-                            function(output) {
-                                if (output.status === "success") {
-                                    search_aborted = true;
-                                    redirectToMap(unique_id);
-                                }
-                            });
-            }
-            
-            function clearFallbackInterval () {
-                if(typeof check_fallback_interval !== 'undefined' 
-                        && check_fallback_interval !== null) {
-                    window.clearInterval(check_fallback_interval);
-                }
-            }
-            
-            function redirectToIndex() {
-                clearFallbackInterval();
-                $("#progress")
-                        .html('Your search request did not contain any data. Please use the search box on the <a class="underline" href="index">index page</a> to search/create a map. You will be redirected there in 10 seconds.<br><br>If you think that there is something wrong with our site, please let us know at <br><a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>');
-                window.setTimeout(function() { window.location = "index"; }, 10000);
-                
-            }
-            
-            function redirectToMap(id) {
-                $("#progressbar").progressbar("option", "value", 100);
-                window.clearTimeout(progessbar_timeout);
-                window.location.replace("map/" + id);
-            }
-            
-            function unboxPostData(post_data, service) {
-                if (service === "base") {
-                    var base_search_string = "https://base-search.net/Search/Results?"
-                            + ((getPostData(post_data, "sorting", "string") === "most-recent") ? ("sort=dcyear_sort+desc&") : (""))
-                            + "refid=okmaps&type0[]=all&lookfor0[]=" + getPostData(post_data, "q", "string")
-                            + "&type0[]=tit&lookfor0[]=&type0[]=aut&lookfor0[]=&type0[]=subj&lookfor0[]=&type0[]=url&lookfor0[]=&offset=10&ling=0&type0[]=country"
-                            + "&lookfor0[]=&daterange=year&yearfrom=" + getPostData(post_data, "from", "string").substr(0, 4) + "&yearto=" + getPostData(post_data, "to", "string").substr(0, 4)
-                            + "&type1[]=doctype" + createDoctypeString(getPostData(post_data, "document_types", "array"), service)
-                            + "&allrights=all&type2[]=rights&lookfor2[]=CC-*&lookfor2[]=CC-BY&lookfor2[]=CC-BY-SA&lookfor2[]=CC-BY-ND&lookfor2[]=CC-BY-NC&lookfor2[]=CC-BY-NC-SA&lookfor2[]=CC-BY-NC-ND&lookfor2[]=PD&lookfor2[]=CC0&lookfor2[]=PDM&type3[]=access&lookfor3[]=1&lookfor3[]=0&lookfor3[]=2&name=&join=AND&bool0[]=AND&bool1[]=OR&bool2[]=OR&bool3[]=OR&newsearch=1";
-
-                    return base_search_string;
-                } else if (service === "pubmed") {
-
-                    var pubmed_string = "https://www.ncbi.nlm.nih.gov/pubmed?"
-                            + "term=((" + getPostData(post_data, "q", "string") + "%20AND%20(%22"
-                            + getPostData(post_data, "from", "string") + "%22%5BDate%20-%20Publication%5D%20%3A%20%22" + getPostData(post_data, "to", "string") + "%22%5BDate%20-%20Publication%5D))"
-                            + "%20AND%20((" + createDoctypeString(getPostData(post_data, "article_types", "array"), service) + "))";
-
-                    return pubmed_string;
-                }
-
-            }
+            // Everything related to the request to the server
             
             function getPostData(post_data, field, type) {
                 if(!(field in post_data) || post_data[field] === 'undefined') {
@@ -173,19 +153,47 @@ if(!empty($_POST)) {
                 
                 return(post_data[field]);               
             }
-
-            function createDoctypeString(doctypes, service) {
-                var doctypes_string = "";
-                doctypes.forEach(function (doctype) {
-                    if (service === "base")
-                        doctypes_string += "&lookfor1[]=" + doctype;
-                    else if (service === "pubmed")
-                        doctypes_string += "%22" + doctype + "%22%5BPublication%20Type%5D%20OR";
-
-                });
-                return doctypes_string;
+            
+            function fallbackCheck() {
+                
+                // Do not start fallback check if an error occurred
+                if(error_occurred) {
+                    return;
+                }
+                
+                $.getJSON("<?php echo $HEADSTART_URL ?>server/services/getLastVersion.php?vis_id=" + unique_id,
+                            function(output) {
+                                if (output.status === "success") {
+                                    search_aborted = true;
+                                    redirectToMap(unique_id);
+                                }
+                            });
             }
             
+            function clearFallbackInterval () {
+                if(typeof check_fallback_interval !== 'undefined' 
+                        && check_fallback_interval !== null) {
+                    window.clearInterval(check_fallback_interval);
+                } else {
+                    console.log("Fallback interval not defined");
+                }
+            }
+            
+            // An error occurred
+            function errorOccurred() {
+                error_occurred = true;
+                console.log("An error occurred while creating the map");
+                clearFallbackInterval();
+                $("#active_state").addClass("nodisplay");
+                $("#error_state").removeClass("nodisplay")
+            }
+            
+            function redirectToMap(id) {
+                $("#progressbar").progressbar("option", "value", 100);
+                window.clearTimeout(progessbar_timeout);
+                window.location.replace("map/" + id);
+            }
+                        
             var getSearchTermShort = function (search_term) {
                 return search_term.length > max_length_search_term_short 
                             ? search_term.substr(0, max_length_search_term_short) + "..." 
@@ -213,8 +221,8 @@ if(!empty($_POST)) {
                             redirectToMap(output.id);
 
                         } else {
-                            clearFallbackInterval();
-                            
+                            errorOccurred();
+
                             let search_string = "";
                             
                             try {
@@ -223,7 +231,7 @@ if(!empty($_POST)) {
                                 console.log("An error ocurred when creating the search string");
                             }
 
-                            $("#progress").html("Sorry! We could not create a knowledge map for your search term. Most likely there were not enough results."
+                            $("#error-title").html("Sorry! We could not create a knowledge map for your search term. Most likely there were not enough results."
                                     + ((search_string !== "")
                                         ?("<br> You can <a href=\"" + search_string + "\" target=\"_blank\">check out your search on " + ((service === "base") ? ("BASE") : ("PubMed")) + "</a> or <a href=\"index.php\">go back and try again.</a>")
                                         :("<br> Please <a href=\"index.php\">go back and try again.</a>"))
@@ -239,41 +247,39 @@ if(!empty($_POST)) {
                         if(search_aborted)
                             return;
                         
-                        clearFallbackInterval();
+                        errorOccurred();
 
-                        console.log("error");
+                        
                         if(xhr.status === 0) {
-                            $("#progress")
+                            $("#error-title")
                                     .html('Error connecting to the server. It seems that you have lost your Internet connection or the connection was reset. You can try again by <a class="underline" style="cursor:pointer" onClick="window.location.reload();">refreshing this page</a>.');
                         } else {
-                            $("#progress").html("Sorry! Something went wrong. Please <a href=\"index.php\">try again</a> in a few minutes. <br><br>If the error persists, please let us know at <a href=\"mailto:info@openknowledgemaps.org\">info@openknowledgemaps.org</a>.");
+                            $("#error-title").html("Sorry! Something went wrong. Please <a href=\"index.php\">try again</a> in a few minutes. <br><br>If the error persists, please let us know at <a href=\"mailto:info@openknowledgemaps.org\">info@openknowledgemaps.org</a>.");
                         }
 
                     })
             
             }
             
-            <?php
-                if(isset($post_data)) {
-                    echo "var post_data = " . $post_data . ";\n";
-                }
+            function redirectToIndex() {
+                $("#error-title")
+                        .html('Your search request did not contain any data. Please use the search box on the <a class="underline" href="index">index page</a> to search/create a map. You will be redirected there in 10 seconds.<br><br>If you think that there is something wrong with our site, please let us know at <br><a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>');
+                window.setTimeout(function() { window.location = "index"; }, 10000);
                 
-                if(isset($unique_id)) {
-                    echo 'var unique_id = "' . $unique_id . '";';
-                }
-            ?>
+            }
             
             //If the page is called without any data, redirect to index page
             if(typeof post_data === "undefined") {
+                errorOccurred();
                 redirectToIndex();
             }
 
             var script = "";
             var vis_page = "";
-            var service = "<?php echo (isset($_GET["service"])?($_GET["service"]):("")) ?>";
-            var milliseconds = 500;
+            var milliseconds_progressbar = 500;
 
             var search_aborted = false;
+            var error_occurred = false;
 
             switch (service) {
                 case "doaj":
@@ -282,17 +288,17 @@ if(!empty($_POST)) {
 
                 case "plos":
                     script = "searchPLOS.php";
-                    milliseconds = 600;
+                    milliseconds_progressbar = 600;
                     break;
 
                 case "pubmed":
                     script = "searchPubmed.php";
-                    milliseconds = 800;
+                    milliseconds_progressbar = 800;
                     break;
 
                 case "base":
                     script = "searchBASE.php";
-                    milliseconds = 800;
+                    milliseconds_progressbar = 800;
                     break;
 
                 default:
@@ -315,14 +321,55 @@ if(!empty($_POST)) {
         </script>
         
         <script>
-            // Everything related to the progress bar
+            // Everything related to error messaging apart from translating 
+            // error descriptions/possible reasons
+            
+            function unboxPostData(post_data, service) {
+                if (service === "base") {
+                    var base_search_string = "https://base-search.net/Search/Results?"
+                            + ((getPostData(post_data, "sorting", "string") === "most-recent") ? ("sort=dcyear_sort+desc&") : (""))
+                            + "refid=okmaps&type0[]=all&lookfor0[]=" + getPostData(post_data, "q", "string")
+                            + "&type0[]=tit&lookfor0[]=&type0[]=aut&lookfor0[]=&type0[]=subj&lookfor0[]=&type0[]=url&lookfor0[]=&offset=10&ling=0&type0[]=country"
+                            + "&lookfor0[]=&daterange=year&yearfrom=" + getPostData(post_data, "from", "string").substr(0, 4) + "&yearto=" + getPostData(post_data, "to", "string").substr(0, 4)
+                            + "&type1[]=doctype" + createDoctypeString(getPostData(post_data, "document_types", "array"), service)
+                            + "&allrights=all&type2[]=rights&lookfor2[]=CC-*&lookfor2[]=CC-BY&lookfor2[]=CC-BY-SA&lookfor2[]=CC-BY-ND&lookfor2[]=CC-BY-NC&lookfor2[]=CC-BY-NC-SA&lookfor2[]=CC-BY-NC-ND&lookfor2[]=PD&lookfor2[]=CC0&lookfor2[]=PDM&type3[]=access&lookfor3[]=1&lookfor3[]=0&lookfor3[]=2&name=&join=AND&bool0[]=AND&bool1[]=OR&bool2[]=OR&bool3[]=OR&newsearch=1";
+
+                    return base_search_string;
+                } else if (service === "pubmed") {
+
+                    var pubmed_string = "https://www.ncbi.nlm.nih.gov/pubmed?"
+                            + "term=((" + getPostData(post_data, "q", "string") + "%20AND%20(%22"
+                            + getPostData(post_data, "from", "string") + "%22%5BDate%20-%20Publication%5D%20%3A%20%22" + getPostData(post_data, "to", "string") + "%22%5BDate%20-%20Publication%5D))"
+                            + "%20AND%20((" + createDoctypeString(getPostData(post_data, "article_types", "array"), service) + "))";
+
+                    return pubmed_string;
+                }
+
+            }
+
+            function createDoctypeString(doctypes, service) {
+                var doctypes_string = "";
+                doctypes.forEach(function (doctype) {
+                    if (service === "base")
+                        doctypes_string += "&lookfor1[]=" + doctype;
+                    else if (service === "pubmed")
+                        doctypes_string += "%22" + doctype + "%22%5BPublication%20Type%5D%20OR";
+
+                });
+                return doctypes_string;
+            }
+            
+        </script>
+                                                
+        <script>
+            // Everything related to the progress bar apart from global settings
 
             function tick_function() {
 
                 var value = $("#progressbar").progressbar("option", "value");
                 value += tick_increment;
                 $("#progressbar").progressbar("option", "value", value);
-                progessbar_timeout = window.setTimeout(tick_function, tick_interval * milliseconds);
+                progessbar_timeout = window.setTimeout(tick_function, tick_interval * milliseconds_progressbar);
 
                 if (value >= 100) {
                     $("#status").html("<span style='color:red'>Creating your visualization takes longer than expected. Please stay tuned!</span>")
@@ -331,13 +378,13 @@ if(!empty($_POST)) {
 
             };
             
-            var tick_interval = 1;
-            var tick_increment = 2;
+            const tick_interval = 1;
+            const tick_increment = 2;
             
             $("#progressbar").progressbar();
             $("#progressbar").progressbar("value", 2);
                        
-            var progessbar_timeout = window.setTimeout(tick_function, tick_interval * milliseconds);
+            var progessbar_timeout = window.setTimeout(tick_function, tick_interval * milliseconds_progressbar);
            
         </script>
                                                 
