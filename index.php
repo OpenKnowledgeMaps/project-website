f6aa8f6043def89d258d948108f638e2614830f2
tanteuschi <maxi@openknowledgemaps.org>
2020-07-20 14:50:59 +0200

    section on index page to highlight our integrations (covis, viper) and open science projects

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en" xmlns:fb="http://ogp.me/ns/fb#">
     <head>
         <base href="<?php echo $SITE_URL ?>">
-        <?php 
+        <?php
         $override_labels = array(
             "title" => "Open Knowledge Maps - A visual interface to the world&#39;s scientific knowledge"
             , "description" => "Start your literature search here: get an overview of a research topic, find relevant papers, and identify important concepts."
@@ -26,25 +26,26 @@
         ?>
 
         <a name="top"></a>
-        
+
         <a style="padding-top:160px;" name="search"></a>
         <?php include($COMPONENTS_PATH . 'search-box.php') ?>
 
-        
+
         <span class="anchor" id="okmmission"></span>
 
         <?php include($COMPONENTS_PATH . 'mission.php') ?>
         <?php include($COMPONENTS_PATH . 'featuredin.php') ?>
-        
-        
+
+
 
         <span class="anchor" id="feedback"></span>
-        
+        <?php include($COMPONENTS_PATH . 'integrations.php') ?>
         <!-- this stream is STATIC -->
         <?php
         $COMMENT_TITLE = "What our users say";
         $COMMENT_IMAGES_URL = "./img/comments/";
         include($COMPONENTS_PATH . 'commentstream.php');
         ?>
-<?php include($COMPONENTS_PATH . 'workshop-teaser.php') ?>
+
+        <?php include($COMPONENTS_PATH . 'workshop-teaser.php') ?>
         <?php include($COMPONENTS_PATH . 'footer.php'); ?>
