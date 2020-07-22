5aa23dcf2f6cf4ecc4a98e098e6fcd37d24e3056
pkraker <pkraker@openknowledgemaps.org>
2020-07-22 18:25:38 +0200

    More changes

--- components/donation-purposes.php
+++ components/donation-purposes.php
@@ -1,1 +1,107 @@
-<!--<div id="mission">    <div id="more-info">                <h2 class="h2reverse" style="padding: 0 20px 20px;">With your donation you contribute to</h2>        <a target="_blank" href="./about">            <div class="vision" style="min-height: 20px;">                <img src="./img/donation/open.png" style="margin-bottom: 20px;">                <h2 style="text-transform: none;">Free and open discovery for everyone</h2>                <p>Open Knowledge Maps is the world’s largest visual search engine for scientific knowledge. We dramatically increase the visibility of                     research findings for science and society alike.                    <br><br><span style="font-weight: bold;">learn more about us</span>                </p>                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>                    </div>        </a>        <a target="_blank" href="https://vimeo.com/188647919">            <div class="vision" style="min-height: 20px;">                <img src="./img/donation/share.png" style="margin-bottom: 20px;">                <h2 style="text-transform: none;">Turning discovery into a collaborative process</h2>                <p>Our vision is to enable collaborative editing of our knowledge maps.                     Just imagine, how useful it would be to combine artificial intelligence with input from experts in the field.                    <br><br><span style="font-weight: bold;">watch this video to find out more</span>                </p>                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>            </div>        </a>        <a target="_blank" href="https://github.com/OpenKnowledgeMaps">            <div class="vision" style="min-height: 20px;">                <img src="./img/donation/opensource.png" style="margin-bottom: 20px;">                <h2 style="text-transform: none;">Open source software for an open science</h2>                <p>Don't leave it up to closed systems. Everyone should be able to benefit from scientific knowledge. To reach this goal                     we need open source non-profit discovery systems for an open science.                    <br><br><span style="font-weight: bold;">check out our github repo</span>                </p>                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>            </div>        </a>    </div></div>--><div id="mission" style="background-color: white;">    <div id="more-info">        <h2 class="h2reverse" style="padding: 0 20px 20px;">With your donation you contribute to</h2>        <div style="margin: 0px auto; max-width: 900px; width: 100%;">            <div class="project-facts" style="margin: 20px 0 0;">                <div class="project-round">                                        <a href="./about">                        <!--<img class="hoverlink mobile-img" style="margin-bottom: 20px;" src="./img/integrations/covis-mobil.png">-->                        <img class="hoverlink" src="./img/donation/open-round.png">                    </a>                </div>                <div class="project-d">                    <a href="./about" class="underline"><h3 class="item-title">Free and open discovery for everyone</h3></a>                    <p>Open Knowledge Maps is the world’s largest visual search engine for scientific knowledge. We dramatically increase the visibility of research findings for science and society alike.                    </p>                </div>            </div>            <div class="project-facts" style="margin: 20px 0 0;">                <div class="project-round">                    <a target="_blank" href="https://vimeo.com/188647919">                        <!--<img class="hoverlink mobile-img" style="margin-bottom: 20px;" src="./img/integrations/viper.png">-->                        <img class="hoverlink" style="" src="./img/donation/share-round.png">                    </a>                </div>                <div class="project-d">                    <a target="_blank" href="https://vimeo.com/188647919" class="underline"><h3 class="item-title">Turning discovery into a collaborative process</h3></a>                    <p>Our vision is to enable collaborative editing of our knowledge maps.                     Just imagine, how useful it would be to combine artificial intelligence with input from experts in the field.                    </p>                </div>            </div>            <div class="project-facts" style="margin: 20px 0 0;">                <div class="project-round">                    <a target="_blank" href="https://github.com/OpenKnowledgeMaps">                        <img class="hoverlink" src="./img/donation/openscience-round.png">                        <!--<img class="hoverlink mobile-img" style="margin-bottom: 20px;" src="./img/integrations/all-projects-mobile.png">-->                    </a>                </div>                <div class="project-d">                    <a target="_blank" href="https://github.com/OpenKnowledgeMaps" class="underline"><h3 class="item-title">Open source software for an open science</h3></a>                    <p>Don't leave it up to closed systems. Everyone should be able to benefit from scientific knowledge. To reach this goal we need open source non-profit discovery systems for an open science.                    </p>                </div>            </div>        </div>    </div></div>
+<!--<div id="mission">
+    <div id="more-info">
+        
+        <h2 class="h2reverse" style="padding: 0 20px 20px;">With your donation you contribute to</h2>
+
+        <a target="_blank" href="./about">
+            <div class="vision" style="min-height: 20px;">
+                <img src="./img/donation/open.png" style="margin-bottom: 20px;">
+                <h2 style="text-transform: none;">Free and open discovery for everyone</h2>
+                <p>Open Knowledge Maps is the world’s largest visual search engine for scientific knowledge. We dramatically increase the visibility of 
+                    research findings for science and society alike.
+                    <br><br><span style="font-weight: bold;">learn more about us</span>
+                </p>
+                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>        
+            </div>
+        </a>
+
+        <a target="_blank" href="https://vimeo.com/188647919">
+            <div class="vision" style="min-height: 20px;">
+                <img src="./img/donation/share.png" style="margin-bottom: 20px;">
+                <h2 style="text-transform: none;">Turning discovery into a collaborative process</h2>
+                <p>Our vision is to enable collaborative editing of our knowledge maps. 
+                    Just imagine, how useful it would be to combine artificial intelligence with input from experts in the field.
+                    <br><br><span style="font-weight: bold;">watch this video to find out more</span>
+                </p>
+                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+            </div>
+        </a>
+
+        <a target="_blank" href="https://github.com/OpenKnowledgeMaps">
+            <div class="vision" style="min-height: 20px;">
+                <img src="./img/donation/opensource.png" style="margin-bottom: 20px;">
+                <h2 style="text-transform: none;">Open source software for an open science</h2>
+                <p>Don't leave it up to closed systems. Everyone should be able to benefit from scientific knowledge. To reach this goal 
+                    we need open source non-profit discovery systems for an open science.
+                    <br><br><span style="font-weight: bold;">check out our github repo</span>
+                </p>
+                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+            </div>
+        </a>
+
+    </div>
+</div>-->
+
+<div id="mission" style="background-color: white;">
+    <div id="more-info">
+
+        <h2 class="h2reverse" style="padding: 0 20px 20px;">With your donation you contribute to</h2>
+
+
+        <div style="margin: 0px auto; max-width: 900px; width: 100%;">
+
+            <div class="project-facts" style="margin: 20px 0 0;">
+
+                <div class="project-round">
+                    
+                    <a href="./about">
+                        <!--<img class="hoverlink mobile-img" style="margin-bottom: 20px;" src="./img/integrations/covis-mobil.png">-->
+                        <img class="hoverlink" src="./img/donation/open-round.png">
+                    </a>
+                </div>
+
+                <div class="project-d">
+                    <a href="./about" class="underline"><h3 class="item-title">Free and open discovery for everyone</h3></a>
+                    <p>Open Knowledge Maps is the world’s largest visual search engine for scientific knowledge. We dramatically increase the visibility of research findings for science and society alike.
+                    </p>
+                </div>
+
+            </div>
+
+            <div class="project-facts" style="margin: 20px 0 0;">
+
+                <div class="project-round">
+                    <a target="_blank" href="https://vimeo.com/188647919">
+                        <!--<img class="hoverlink mobile-img" style="margin-bottom: 20px;" src="./img/integrations/viper.png">-->
+                        <img class="hoverlink" style="" src="./img/donation/share-round.png">
+                    </a>
+                </div>
+
+                <div class="project-d">
+                    <a target="_blank" href="https://vimeo.com/188647919" class="underline"><h3 class="item-title">Turning discovery into a collaborative process</h3></a>
+                    <p>Our vision is to enable collaborative editing of our knowledge maps. 
+                    Just imagine, how useful it would be to combine artificial intelligence with input from experts in the field.
+                    </p>
+                </div>
+
+            </div>
+
+            <div class="project-facts" style="margin: 20px 0 0;">
+
+                <div class="project-round">
+                    <a target="_blank" href="https://github.com/OpenKnowledgeMaps">
+                        <img class="hoverlink" src="./img/donation/openscience-round.png">
+                        <!--<img class="hoverlink mobile-img" style="margin-bottom: 20px;" src="./img/integrations/all-projects-mobile.png">-->
+                    </a>
+                </div>
+
+                <div class="project-d">
+                    <a target="_blank" href="https://github.com/OpenKnowledgeMaps" class="underline"><h3 class="item-title">Open source software for an open science</h3></a>
+                    <p>Don't leave it up to closed systems. Everyone should be able to benefit from scientific knowledge. To reach this goal we need open source non-profit discovery systems for an open science.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
