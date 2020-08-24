306b55d2ed499d98ff5c5873cf5872f82926359e
tanteuschi <maxi@openknowledgemaps.org>
2020-08-24 16:37:09 +0200

    added portuguese translations in material section, created tags for languages, updated pics

--- components/materials.php
+++ components/materials.php
@@ -1,1 +1,61 @@
-<div id="mission">    <span class="anchor" id="training-materials"></span>    <div id="more-info">        <h2 class="h2reverse">Training materials</h2>        <p class="supportertext">            Would you like to introduce OKMaps to your community? Now you can! Here are all the necessary materials             to run a workshop, including a fun and fast paced game to get to know our tool.        </p>        <a target="_blank" href="https://drive.google.com/open?id=1TuqJuiYC1LRAAd0iParqgmohWglWOjUu">            <div class="vision" style="min-height: 20px;">                <p><img src="./img/materials/presentation-eng-spanish.jpg" alt='Open Knowledge Maps presentation and English or Spanish'></p>                <h2>Open Knowledge Maps presentation (in English or Spanish)</h2>                <p>You can use these slides to inform your community about open discovery and Open Knowledge Maps.                    <br><br><span style="font-weight: bold;">view and download</span>                </p>                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>            </div>        </a>        <a target="_blank" href="https://drive.google.com/open?id=14avm5ub50sH6PU5TWpHD4TRaeuV1NXzh">            <div class="vision" style="min-height: 20px;">                <p><img src="./img/materials/okmaps-scavengerhunt.jpg" alt='Open Knowledge Maps Scavenger Hunt Workshop Materials'></p>                <h2>Open Knowledge Maps Scavenger Hunt</h2>                <p>Play a fun fast paced game with your colleagues to get to know Open Knowledge Maps.                    <br><br><span style="font-weight: bold;">view and download</span>                </p>                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>            </div>        </a>                <a target="_blank" href="https://drive.google.com/open?id=1TuqJuiYC1LRAAd0iParqgmohWglWOjUu">            <div class="vision" style="min-height: 20px;">                <p><img src="./img/materials/academic-seo.png" alt='Academic SEO Workshop Materials'></p>                <h2>Academic SEO (in English)<!--: Improve the visibility of your research online (in English)--></h2>                <p>This workshop includes practical tips on how to improve the visibility of your research online<!--“How do I get my research to show up                     in search engines and discovery tools?”--> and an introduction to Open Knowledge Maps.                    <br><br><span style="font-weight: bold;">view and download</span>                </p>                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>            </div>        </a>        <a target="_blank" href="https://drive.google.com/open?id=1WrZSsME3IkAhcVu-eV6Q8lKvyKE8fU_5">            <div class="vision" style="min-height: 20px;">                <p><img src="./img/materials/okmaps-posters.jpg" alt='Open Knowledge Maps Poster'></p>                <h2>Open Knowledge Maps Poster</h2>                <p>Print it, hang it, love it - and use it to inform your community about Open Knowledge Maps.                    <br><br><span style="font-weight: bold;">view and download</span>                </p>                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>            </div>        </a>    </div></div>
+<div id="mission">
+    <span class="anchor" id="training-materials"></span>
+
+    <div id="more-info">
+        <h2 class="h2reverse">Training materials</h2>
+        <p class="supportertext">
+            Would you like to introduce OKMaps to your community? Now you can! Here are all the necessary materials 
+            to run a workshop, including a fun and fast paced game to get to know our tool.
+        </p>
+
+        <a target="_blank" href="https://drive.google.com/open?id=1TuqJuiYC1LRAAd0iParqgmohWglWOjUu">
+            <div class="vision" style="min-height: 20px;">
+                <p><img src="./img/materials/presentation-eng-spanish.jpg" alt='Open Knowledge Maps presentation and English or Spanish'></p>
+                <div class="tag-div"><span class="tag">English</span> <span class="tag">Spanish</span> <span class="tag">Portuguese</span></div>
+                <h2>Open Knowledge Maps presentation</h2>
+                <p>You can use these slides to inform your community about open discovery and Open Knowledge Maps.
+                    <br><br><span style="font-weight: bold;">view and download</span>
+                </p>
+                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+            </div>
+        </a>
+
+        <a target="_blank" href="https://drive.google.com/open?id=14avm5ub50sH6PU5TWpHD4TRaeuV1NXzh">
+            <div class="vision" style="min-height: 20px;">
+                <p><img src="./img/materials/okmaps-scavengerhunt.jpg" alt='Open Knowledge Maps Scavenger Hunt Workshop Materials'></p>
+                <div class="tag-div"><span class="tag">English</span></div>
+                <h2>Open Knowledge Maps Scavenger Hunt</h2>
+                <p>Play a fun fast paced game with your colleagues to get to know Open Knowledge Maps.
+                    <br><br><span style="font-weight: bold;">view and download</span>
+                </p>
+                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+            </div>
+        </a>
+        
+        <a target="_blank" href="https://drive.google.com/open?id=1TuqJuiYC1LRAAd0iParqgmohWglWOjUu">
+            <div class="vision" style="min-height: 20px;">
+                <p><img src="./img/materials/academic-seo.png" alt='Academic SEO Workshop Materials'></p>
+                <div class="tag-div"><span class="tag">English</span> <span class="tag">Portuguese</span></div>
+                <h2>Academic SEO<!--: Improve the visibility of your research online (in English)--></h2>
+                <p>This workshop includes practical tips on how to improve the visibility of your research online<!--“How do I get my research to show up 
+                    in search engines and discovery tools?”--> and an introduction to Open Knowledge Maps.
+                    <br><br><span style="font-weight: bold;">view and download</span>
+                </p>
+                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+            </div>
+        </a>
+
+        <a target="_blank" href="https://drive.google.com/open?id=1WrZSsME3IkAhcVu-eV6Q8lKvyKE8fU_5">
+            <div class="vision" style="min-height: 20px;">
+                <p><img src="./img/materials/okmaps-posters.jpg" alt='Open Knowledge Maps Poster'></p>
+                <div class="tag-div"><span class="tag">English</span></div>
+                <h2>Open Knowledge Maps Poster</h2>
+                <p>Print it, hang it, love it - and use it to inform your community about Open Knowledge Maps.
+                    <br><br><span style="font-weight: bold;">view and download</span>
+                </p>
+                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+            </div>
+        </a>
+
+    </div>
+</div>
