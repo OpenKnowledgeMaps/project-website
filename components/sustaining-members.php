4611ef6079a6689bba2419fe3c923d6cb6491c8f
tanteuschi <maxi@openknowledgemaps.org>
2020-09-08 11:27:00 +0200

    added new supporting member eth

--- components/sustaining-members.php
+++ components/sustaining-members.php
@@ -1,2 +1,2 @@
-<a href="https://www.library.ethz.ch" target="_blank"><img src="./img/supporting-members/bos/ETH_Fremdmedienlogos_ETH-Bibliothek_horizontal.png" alt="Logo ETH Zürich ETH Bibliothek"></a>
 <a href="https://www.tuwien.at/en/library/" target="_blank"><img src="./img/supporting-members/bos/TU_Wien_Bibliothek_RGB.png" alt="Logo TU Wien Bibliothek"></a>
+<a href="https://www.library.ethz.ch" target="_blank"><img src="./img/supporting-members/bos/ETH_Fremdmedienlogos_ETH-Bibliothek_horizontal.png" alt="Logo ETH Zürich ETH Bibliothek"></a>
