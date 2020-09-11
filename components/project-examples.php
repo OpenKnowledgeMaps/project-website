e1f1b730e31f9815494b9183614fb7e4e8f552ca
pkraker <pkraker@openknowledgemaps.org>
2020-09-11 18:31:26 +0200

    Fix line endings

--- components/project-examples.php
+++ components/project-examples.php
@@ -1,1 +1,123 @@
-<div id="divhow" class="work-with-us">    <div class="faq">        <h2 class="h2reverse">Past and present projects</h2>    </div>    <div class="project-examples">        <div class="project-card">            <div class="project-pic">                <a target="_blank" href="https://openknowledgemaps.org/covis"><img class="hoverlink desktop-img" src="./img/project-examples/covis-desktop.png" alt='CoVis project funded by EOSCSecretariat'></a>                <a target="_blank" href="https://openknowledgemaps.org/covis"><img class="hoverlink mobile-img" src="./img/project-examples/covis.png" alt='CoVis project funded by EOSCSecretariat'></a>            </div>            <div class="project-description">                <h3>CoVis: A curated, collaborative & visual knowledge base for COVID-19 research</h3>                <p class="project-facts">CoVis is an ongoing project in collaboration with ReFigure funded by EOSCSecretariat. CoVis enables scientists to kick-start their COVID-19 research by providing a curated knowledge map of seminal works on COVID-19 from eight critical areas of biomedical research. The knowledge map is constantly evolving thanks to the collective editing of subject-matter experts.</p>                <ul>                    <li><span class="bold">Funder:</span> EOSCSecretariat</li>                    <li><span class="bold">Partners:</span> ReFigure</li>                    <li><span class="bold">Runtime:</span> 2020 <span class="freeadmission">*running</span></li>                    <li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://openknowledgemaps.org/covis">CoVis</a></li>                </ul>            </div>        </div>                <div class="project-card">            <div class="project-pic">                <img class="desktop-img" src="./img/project-examples/triple-desktop.png" alt='Triple EU project funded under the Horizon 2020 framework'>                <img class="mobile-img" src="./img/project-examples/triple.png" alt='Triple EU project funded under the Horizon 2020 framework'>            </div>            <div class="project-description">                <h3>TRIPLE: The multilingual discovery platform for the social sciences and humanities.</h3>                <p class="project-facts">TRIPLE is a EU project funded under the Horizon 2020 framework. TRIPLE's goal is to create a comprehensive free and open discovery solution for the social sciences and humanities (SSH). Under the lead of Huma-Num (CNRS), a consortium of 18 partners from 12 countries will develop TRIPLE as a core service of OPERAS and the European Open Science Cloud (EOSC).</p>                <ul>                    <li><span class="bold">Funder:</span> The project is funded by the European Commission, under Grant Agreement No. 863420</li>                    <li><span class="bold">Partners:</span> CESSDA ERIC, CLARIN ERIC, CNR, CNRS, Coimbra University, DARIAH ERIC, EGI, EKT, IBL PAN, Know-Center, Lexical Computing, Max Weber Stiftung, MEOH, Net7, Nuromedia, OAPEN, The University Court of the University of Abertay Dundee</li>                    <li><span class="bold">Runtime:</span> 2019-2023 <span class="freeadmission">*running</span></li>                </ul>            </div>        </div>                <div class="project-card">            <div class="project-pic">                <img class="desktop-img" src="./img/project-examples/linkedcat-desktop.png" alt='Linked Cat+ project funded by the Austrian Academy of Sciences'>                <img class="mobile-img" src="./img/project-examples/linkedcat.png" alt='Linked Cat+ project funded by the Austrian Academy of Sciences'>            </div>            <div class="project-description">                <h3>Linked Cat+: Explore half a century of knowledge production at the Academy</h3>                <p class="project-facts">LinkedCat+ is an ongoing project funded by the Austrian Academy of Sciences (OeAW)                     to make the early proceedings of the OeAW accessible, discoverable and reusable.  LinkedCat+ will provide                     open access fulltexts and bibliographic catalogue data as linked open data, and allow for visual exploration                     of this data.</p>                <ul>                    <li><span class="bold">Funder:</span> Austrian Academy of Science (OeAW)</li>                    <li><span class="bold">Partners:</span> BAS:IS (Library, Archive, OeAW Collections), Austrian Centre for Digital Humanities (ACDH)</li>                    <li><span class="bold">Runtime:</span> 2018-2020 <span class="freeadmission">*running</span></li>                </ul>            </div>        </div>                <div class="project-card">            <div class="project-pic">                <!--<a target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions">-->                <img class="desktop-img" src="./img/project-examples/crisvis2-desktop.png" alt='CRIS VIS II ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'>                <img class="mobile-img" src="./img/project-examples/crisvis2.png" alt='CRIS VIS II ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'>            </div>            <div class="project-description">                <h3>CRIS II Visualization</h3>                <p class="project-facts">CRIS Vis II is an ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft (LBG). The second iteration of CRIS Vis enables researchers, practitioners and citizens to explore crowd-sourced research questions related to accidental injuries.                </p>                <ul>                    <li><span class="bold">Partner:</span> Open Innovation in Science Center (OIS Center)</li>                    <li><span class="bold">Funder:</span> Ludwig Boltzmann Gesellschaft</li>                    <li><span class="bold">Runtime:</span> 2019 <span class="freeadmission">*running</span></li>                    <!--<li style="padding-bottom: 10px;"><span class="bold">Completed:</span> June 2018</li>-->                    <!--<li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions">CRIS II Visualization</a></li>-->                </ul>            </div>        </div>        <div class="project-card">            <div class="project-pic">                <a target="_blank" href="https://openknowledgemaps.org/viper/"><img class="hoverlink desktop-img" src="./img/project-examples/viper-desktop.png" alt='VIPER - The Visual Project Explore funded by OpenAIRE'></a>                <a target="_blank" href="https://openknowledgemaps.org/viper/"><img class="hoverlink mobile-img" src="./img/project-examples/viper.png" alt='VIPER - The Visual Project Explore funded by OpenAIRE'></a>            </div>            <div class="project-description">                <h3>VIPER - The Visual Project Explorer</h3>                <p class="project-facts">                    VIPER was an OpenAIRE tender project to create a prototype of the Visual Project Explorer that                     provides overviews of over 2.4 million research projects indexed by OpenAIRE. VIPER enables funders,                     institutions and researchers to systematically explore a project’s output, and to understand its impact                     in different areas.                </p>                <ul>                    <li><span class="bold">Funder:</span> OpenAIRE</li>                    <!--<li style="padding-bottom: 10px;"><span class="bold">Completed:</span> May 2018</li>-->                    <li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://openknowledgemaps.org/viper/">VIPER</a></li>                </ul>            </div>        </div>        <div class="project-card">            <div class="project-pic">                <a target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions"><img class="hoverlink desktop-img" src="./img/project-examples/crisvis-desktop.png" alt='CRIS VIS I ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'></a>                <a target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions"><img class="hoverlink mobile-img" src="./img/project-examples/crisvis.png" alt='CRIS VIS I ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'></a>            </div>            <div class="project-description">                <h3>CRIS I Visualization</h3>                <p class="project-facts">CRIS Vis on mental health was a project in collaboration with the Open Innovation in Science Center                     of the Ludwig Boltzmann Gesellschaft (LBG). It enables researchers, practitioners and citizens to explore crowd-sourced                     research questions related to mental health.                </p>                <ul>                    <li><span class="bold">Partner:</span> Open Innovation in Science Center (OIS Center)</li>                    <li><span class="bold">Funder:</span> Ludwig Boltzmann Gesellschaft</li>                    <!--<li style="padding-bottom: 10px;"><span class="bold">Completed:</span> June 2018</li>-->                    <li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions">CRIS I Visualization (in German)</a></li>                </ul>            </div>        </div>    </div></div>
+<div id="divhow" class="work-with-us">
+
+    <div class="faq">
+        <h2 class="h2reverse">Past and present projects</h2>
+    </div>
+
+    <div class="project-examples">
+
+        <div class="project-card">
+            <div class="project-pic">
+                <a target="_blank" href="https://openknowledgemaps.org/covis"><img class="hoverlink desktop-img" src="./img/project-examples/covis-desktop.png" alt='CoVis project funded by EOSCSecretariat'></a>
+                <a target="_blank" href="https://openknowledgemaps.org/covis"><img class="hoverlink mobile-img" src="./img/project-examples/covis.png" alt='CoVis project funded by EOSCSecretariat'></a>
+            </div>
+            <div class="project-description">
+                <h3>CoVis: A curated, collaborative & visual knowledge base for COVID-19 research</h3>
+                <p class="project-facts">CoVis is an ongoing project in collaboration with ReFigure funded by EOSCSecretariat. CoVis enables scientists to kick-start their COVID-19 research by providing a curated knowledge map of seminal works on COVID-19 from eight critical areas of biomedical research. The knowledge map is constantly evolving thanks to the collective editing of subject-matter experts.</p>
+                <ul>
+                    <li><span class="bold">Funder:</span> EOSCSecretariat</li>
+                    <li><span class="bold">Partners:</span> ReFigure</li>
+                    <li><span class="bold">Runtime:</span> 2020 <span class="freeadmission">*running</span></li>
+                    <li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://openknowledgemaps.org/covis">CoVis</a></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="project-card">
+            <div class="project-pic">
+                <img class="desktop-img" src="./img/project-examples/triple-desktop.png" alt='Triple EU project funded under the Horizon 2020 framework'>
+                <img class="mobile-img" src="./img/project-examples/triple.png" alt='Triple EU project funded under the Horizon 2020 framework'>
+            </div>
+            <div class="project-description">
+                <h3>TRIPLE: The multilingual discovery platform for the social sciences and humanities.</h3>
+                <p class="project-facts">TRIPLE is a EU project funded under the Horizon 2020 framework. TRIPLE's goal is to create a comprehensive free and open discovery solution for the social sciences and humanities (SSH). Under the lead of Huma-Num (CNRS), a consortium of 18 partners from 12 countries will develop TRIPLE as a core service of OPERAS and the European Open Science Cloud (EOSC).</p>
+                <ul>
+                    <li><span class="bold">Funder:</span> The project is funded by the European Commission, under Grant Agreement No. 863420</li>
+                    <li><span class="bold">Partners:</span> CESSDA ERIC, CLARIN ERIC, CNR, CNRS, Coimbra University, DARIAH ERIC, EGI, EKT, IBL PAN, Know-Center, Lexical Computing, Max Weber Stiftung, MEOH, Net7, Nuromedia, OAPEN, The University Court of the University of Abertay Dundee</li>
+                    <li><span class="bold">Runtime:</span> 2019-2023 <span class="freeadmission">*running</span></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="project-card">
+            <div class="project-pic">
+                <img class="desktop-img" src="./img/project-examples/linkedcat-desktop.png" alt='Linked Cat+ project funded by the Austrian Academy of Sciences'>
+                <img class="mobile-img" src="./img/project-examples/linkedcat.png" alt='Linked Cat+ project funded by the Austrian Academy of Sciences'>
+            </div>
+            <div class="project-description">
+                <h3>Linked Cat+: Explore half a century of knowledge production at the Academy</h3>
+                <p class="project-facts">LinkedCat+ is an ongoing project funded by the Austrian Academy of Sciences (OeAW) 
+                    to make the early proceedings of the OeAW accessible, discoverable and reusable.  LinkedCat+ will provide 
+                    open access fulltexts and bibliographic catalogue data as linked open data, and allow for visual exploration 
+                    of this data.</p>
+                <ul>
+                    <li><span class="bold">Funder:</span> Austrian Academy of Science (OeAW)</li>
+                    <li><span class="bold">Partners:</span> BAS:IS (Library, Archive, OeAW Collections), Austrian Centre for Digital Humanities (ACDH)</li>
+                    <li><span class="bold">Runtime:</span> 2018-2020 <span class="freeadmission">*running</span></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="project-card">
+            <div class="project-pic">
+                <!--<a target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions">-->
+                <img class="desktop-img" src="./img/project-examples/crisvis2-desktop.png" alt='CRIS VIS II ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'>
+                <img class="mobile-img" src="./img/project-examples/crisvis2.png" alt='CRIS VIS II ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'>
+            </div>
+            <div class="project-description">
+                <h3>CRIS II Visualization</h3>
+                <p class="project-facts">CRIS Vis II is an ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft (LBG). The second iteration of CRIS Vis enables researchers, practitioners and citizens to explore crowd-sourced research questions related to accidental injuries.
+                </p>
+                <ul>
+                    <li><span class="bold">Partner:</span> Open Innovation in Science Center (OIS Center)</li>
+                    <li><span class="bold">Funder:</span> Ludwig Boltzmann Gesellschaft</li>
+                    <li><span class="bold">Runtime:</span> 2019 <span class="freeadmission">*running</span></li>
+                    <!--<li style="padding-bottom: 10px;"><span class="bold">Completed:</span> June 2018</li>-->
+                    <!--<li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions">CRIS II Visualization</a></li>-->
+                </ul>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <div class="project-pic">
+                <a target="_blank" href="https://openknowledgemaps.org/viper/"><img class="hoverlink desktop-img" src="./img/project-examples/viper-desktop.png" alt='VIPER - The Visual Project Explore funded by OpenAIRE'></a>
+                <a target="_blank" href="https://openknowledgemaps.org/viper/"><img class="hoverlink mobile-img" src="./img/project-examples/viper.png" alt='VIPER - The Visual Project Explore funded by OpenAIRE'></a>
+            </div>
+            <div class="project-description">
+                <h3>VIPER - The Visual Project Explorer</h3>
+                <p class="project-facts">
+                    VIPER was an OpenAIRE tender project to create a prototype of the Visual Project Explorer that 
+                    provides overviews of over 2.4 million research projects indexed by OpenAIRE. VIPER enables funders, 
+                    institutions and researchers to systematically explore a project’s output, and to understand its impact 
+                    in different areas.
+                </p>
+                <ul>
+                    <li><span class="bold">Funder:</span> OpenAIRE</li>
+                    <!--<li style="padding-bottom: 10px;"><span class="bold">Completed:</span> May 2018</li>-->
+                    <li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://openknowledgemaps.org/viper/">VIPER</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <div class="project-pic">
+                <a target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions"><img class="hoverlink desktop-img" src="./img/project-examples/crisvis-desktop.png" alt='CRIS VIS I ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'></a>
+                <a target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions"><img class="hoverlink mobile-img" src="./img/project-examples/crisvis.png" alt='CRIS VIS I ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'></a>
+            </div>
+            <div class="project-description">
+                <h3>CRIS I Visualization</h3>
+                <p class="project-facts">CRIS Vis on mental health was a project in collaboration with the Open Innovation in Science Center 
+                    of the Ludwig Boltzmann Gesellschaft (LBG). It enables researchers, practitioners and citizens to explore crowd-sourced 
+                    research questions related to mental health.
+                </p>
+                <ul>
+                    <li><span class="bold">Partner:</span> Open Innovation in Science Center (OIS Center)</li>
+                    <li><span class="bold">Funder:</span> Ludwig Boltzmann Gesellschaft</li>
+                    <!--<li style="padding-bottom: 10px;"><span class="bold">Completed:</span> June 2018</li>-->
+                    <li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions">CRIS I Visualization (in German)</a></li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</div>
