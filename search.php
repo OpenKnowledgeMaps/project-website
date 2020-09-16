5b8ce4400fb3be31a96116af3c17faa4e3773f17
tanteuschi <maxi@openknowledgemaps.org>
2020-09-16 17:34:07 +0200

    fixed css issues for search-box on search.php

--- search.php
+++ search.php
@@ -110,7 +110,7 @@ if(!empty($_POST)) {
         ?>
 
         <style>
-            .ui-widget-header {
+            #progressbar .ui-widget-header {
                 background: #e55137;
                 border: 1px solid #DDDDDD;
                 color: #333333;
@@ -199,8 +199,8 @@ if(!empty($_POST)) {
                     title: "Sorry! We could not create a knowledge map."
                     , reason: 'Most likely there were not enough results for your search query: <strong id="search_term_fail"></strong>'
                     , remedy: "<strong>Here are some tips to improve your query:</strong>"
-                    , more_info: 'Alternatively you can <a id="more-info-link_na" target="_blank">check out results for your search query on <span id="more-info-link_service"></span></a>'
-                    , contact: 'If you think that there is something wrong with our service, please let us know at <br><a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>. Make sure you include the search query in your message.'
+                    , more_info: 'Alternatively you can <a id="more-info-link_na" class="underline" target="_blank">check out results for your search query on <span id="more-info-link_service"></span></a>'
+                    , contact: 'If you think that there is something wrong with our service, please let us know at <br><a class="underline" href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>. Make sure you include the search query in your message.'
                     , "resolution": "Try again"
                     , "resolution_link": "index"
                 },
@@ -215,7 +215,7 @@ if(!empty($_POST)) {
                 server_error: {
                     title: "Sorry! An unknown server error occurred."
                     , reason: 'Please <a href="index.php">try again</a> in a few minutes.'
-                    , remedy: 'If the error persists, please let us know at <a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>.'
+                    , remedy: 'If the error persists, please let us know at <a class="underline" href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>.'
                     , "resolution": "Try again"
                     , "resolution_link": "index"
                     
@@ -223,7 +223,7 @@ if(!empty($_POST)) {
                 no_post_data: {
                     title: "Oooups! You should not be here..."
                     , reason: 'Sorry about that. You will be redirected to <a class="underline" href="index">our service</a> in 10 seconds.'
-                    , contact: 'If you think that there is something wrong with our service, please let us know at <br><a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>'
+                    , contact: 'If you think that there is something wrong with our service, please let us know at <br><a class="underline" href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>'
                     , "resolution": "Go to search page"
                     , "resolution_link": "index"
                     
@@ -232,7 +232,7 @@ if(!empty($_POST)) {
                     title: "Your request timed out."
                     , reason: "It seems that you have lost your Internet connection or the request to the data source is taking longer than expected."
                     , remedy: 'You can try again by <a class="underline" style="cursor:pointer" onClick="window.location.reload();">refreshing this page</a>.'
-                    , contact: 'If you think that there is something wrong with our service, please let us know at <br><a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>'
+                    , contact: 'If you think that there is something wrong with our service, please let us know at <br><a class="underline" href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>'
                     , "resolution": "Refresh this page"
                     , "resolution_link": "javascript:location.reload()"
                     
