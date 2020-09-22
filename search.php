332f6765123ef1829bb7cca68f12cc0160d21dc9
pkraker <pkraker@openknowledgemaps.org>
2020-09-22 10:45:26 +0200

    Implements a new error route (PubMed 502 error)

--- search.php
+++ search.php
@@ -213,6 +213,15 @@ if(!empty($_POST)) {
                     , "resolution_link": "javascript:location.reload()"
                     
                 },
+                pubmed_api_fail: {
+                    title: "An unexpected error ocurred while retrieving data from PubMed"
+                    , reason: "We are currently experiencing issues with the PubMed API. We are working to fix them."
+                    , remedy: 'Please <a class="underline" style="cursor:pointer" onClick="window.location.reload();">try again</a> in a few minutes or <a class="underline" style="cursor:pointer" href="index">use the BASE integration</a>, which also covers the articles indexed in PubMed.'
+                    , contact: 'For more information about our service please <a href="https://openknowledgemaps.org/faq">see our FAQs</a>. If you think that there is something wrong with our service, please let us know at <a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>'
+                    , "resolution": "Try again"
+                    , "resolution_link": "javascript:location.reload()"
+                    
+                },
             }
             
             //Set JavaScript values influenced by PHP & error code translations
