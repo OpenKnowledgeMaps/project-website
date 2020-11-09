6253bcfcaee8865b2c1cef97fa2cf4e445145cbc
tanteuschi <maxi@openknowledgemaps.org>
2020-11-09 16:42:23 +0100

    added elife to projects, updated cris vis II link

--- components/project-examples.php
+++ components/project-examples.php
@@ -8,23 +8,6 @@
 
         <div class="project-card">
             <div class="project-pic">
-                <a target="_blank" href="https://openknowledgemaps.org/covis"><img class="hoverlink desktop-img" src="./img/project-examples/covis-desktop.png" alt='CoVis project funded by EOSCSecretariat'></a>
-                <a target="_blank" href="https://openknowledgemaps.org/covis"><img class="hoverlink mobile-img" src="./img/project-examples/covis.png" alt='CoVis project funded by EOSCSecretariat'></a>
-            </div>
-            <div class="project-description">
-                <h3>CoVis: A curated, collaborative & visual knowledge base for COVID-19 research</h3>
-                <p class="project-facts">CoVis is an ongoing project in collaboration with ReFigure funded by EOSCSecretariat. CoVis enables scientists to kick-start their COVID-19 research by providing a curated knowledge map of seminal works on COVID-19 from eight critical areas of biomedical research. The knowledge map is constantly evolving thanks to the collective editing of subject-matter experts.</p>
-                <ul>
-                    <li><span class="bold">Funder:</span> EOSCSecretariat</li>
-                    <li><span class="bold">Partners:</span> ReFigure</li>
-                    <li><span class="bold">Runtime:</span> 2020 <span class="freeadmission">*running</span></li>
-                    <li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://openknowledgemaps.org/covis">CoVis</a></li>
-                </ul>
-            </div>
-        </div>
-        
-        <div class="project-card">
-            <div class="project-pic">
                 <img class="desktop-img" src="./img/project-examples/triple-desktop.png" alt='Triple EU project funded under the Horizon 2020 framework'>
                 <img class="mobile-img" src="./img/project-examples/triple.png" alt='Triple EU project funded under the Horizon 2020 framework'>
             </div>
@@ -39,6 +22,43 @@
             </div>
         </div>
         
+        <div class="project-card">
+            <div class="project-pic">
+                
+                   <img class="desktop-img" src="./img/project-examples/elife-desktop.png" alt='project funded by eLife'>
+                    <img class="mobile-img" src="./img/project-examples/elife.png" alt='project funded by eLife'>
+            </div>
+            <div class="project-description">
+                <h3>Open Knowledge Maps Frontend Refactoring</h3>
+                <p class="project-facts">With eLife's backing, we are performing a necessary refactoring of the Open Knowledge Maps frontend.
+
+The refactoring improves the serviceability, reusability and structure of our frontend. The aim is to make it easier to introduce new functionality crucial to fulfilling important use cases, such as integrating Open Knowledge Maps into user workflows, enabling future integrations (e.g. of dataset indices), and reaching our goal of a collaborative discovery environment.</p>
+                <ul>
+                    <li><span class="bold">Funder:</span> eLife</li>
+                    <li><span class="bold">Runtime:</span> 2020 - 2021 <span class="freeadmission">*running</span></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <div class="project-pic">
+                <a target="_blank" href="https://openknowledgemaps.org/covis">
+                    <img class="hoverlink desktop-img" src="./img/project-examples/covis-desktop.png" alt='CoVis project funded by EOSCSecretariat'>
+                    <img class="hoverlink mobile-img" src="./img/project-examples/covis.png" alt='CoVis project funded by EOSCSecretariat'>
+                </a>
+            </div>
+            <div class="project-description">
+                <h3>CoVis: A curated, collaborative & visual knowledge base for COVID-19 research</h3>
+                <p class="project-facts">CoVis is an ongoing project in collaboration with ReFigure funded by EOSCSecretariat. CoVis enables scientists to kick-start their COVID-19 research by providing a curated knowledge map of seminal works on COVID-19 from eight critical areas of biomedical research. The knowledge map is constantly evolving thanks to the collective editing of subject-matter experts.</p>
+                <ul>
+                    <li><span class="bold">Funder:</span> EOSCSecretariat</li>
+                    <li><span class="bold">Partners:</span> ReFigure</li>
+                    <li><span class="bold">Runtime:</span> 2020 <span class="freeadmission">*running</span></li>
+                    <li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://openknowledgemaps.org/covis">CoVis</a></li>
+                </ul>
+            </div>
+        </div>
+
         <div class="project-card">
             <div class="project-pic">
                 <img class="desktop-img" src="./img/project-examples/linkedcat-desktop.png" alt='Linked Cat+ project funded by the Austrian Academy of Sciences'>
@@ -57,12 +77,13 @@
                 </ul>
             </div>
         </div>
-        
+
         <div class="project-card">
             <div class="project-pic">
-                <!--<a target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions">-->
-                <img class="desktop-img" src="./img/project-examples/crisvis2-desktop.png" alt='CRIS VIS II ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'>
-                <img class="mobile-img" src="./img/project-examples/crisvis2.png" alt='CRIS VIS II ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'>
+                <a target="_blank" href="https://ois.lbg.ac.at/en/cris-2">
+                    <img class="hoverlink desktop-img" src="./img/project-examples/crisvis2-desktop.png" alt='CRIS VIS II ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'>
+                    <img class="hoverlink mobile-img" src="./img/project-examples/crisvis2.png" alt='CRIS VIS II ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'>
+                </a>
             </div>
             <div class="project-description">
                 <h3>CRIS II Visualization</h3>
@@ -71,17 +92,18 @@
                 <ul>
                     <li><span class="bold">Partner:</span> Open Innovation in Science Center (OIS Center)</li>
                     <li><span class="bold">Funder:</span> Ludwig Boltzmann Gesellschaft</li>
-                    <li><span class="bold">Runtime:</span> 2019 <span class="freeadmission">*running</span></li>
-                    <!--<li style="padding-bottom: 10px;"><span class="bold">Completed:</span> June 2018</li>-->
-                    <!--<li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions">CRIS II Visualization</a></li>-->
+
+                    <li><span class="bold">Link:</span> <a class="underline" target="_blank" href="https://ois.lbg.ac.at/en/cris-2">CRIS II Visualization</a></li>
                 </ul>
             </div>
         </div>
 
         <div class="project-card">
             <div class="project-pic">
-                <a target="_blank" href="https://openknowledgemaps.org/viper/"><img class="hoverlink desktop-img" src="./img/project-examples/viper-desktop.png" alt='VIPER - The Visual Project Explore funded by OpenAIRE'></a>
-                <a target="_blank" href="https://openknowledgemaps.org/viper/"><img class="hoverlink mobile-img" src="./img/project-examples/viper.png" alt='VIPER - The Visual Project Explore funded by OpenAIRE'></a>
+                <a target="_blank" href="https://openknowledgemaps.org/viper/">
+                    <img class="hoverlink desktop-img" src="./img/project-examples/viper-desktop.png" alt='VIPER - The Visual Project Explore funded by OpenAIRE'>
+                    <img class="hoverlink mobile-img" src="./img/project-examples/viper.png" alt='VIPER - The Visual Project Explore funded by OpenAIRE'>
+                </a>
             </div>
             <div class="project-description">
                 <h3>VIPER - The Visual Project Explorer</h3>
@@ -101,8 +123,9 @@
 
         <div class="project-card">
             <div class="project-pic">
-                <a target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions"><img class="hoverlink desktop-img" src="./img/project-examples/crisvis-desktop.png" alt='CRIS VIS I ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'></a>
-                <a target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions"><img class="hoverlink mobile-img" src="./img/project-examples/crisvis.png" alt='CRIS VIS I ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'></a>
+                <a target="_blank" href="https://ois.lbg.ac.at/en/cris-I-research-questions"><img class="hoverlink desktop-img" src="./img/project-examples/crisvis-desktop.png" alt='CRIS VIS I ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'>
+                    <img class="hoverlink mobile-img" src="./img/project-examples/crisvis.png" alt='CRIS VIS I ongoing project in collaboration with the Open Innovation in Science Center of the Ludwig Boltzmann Gesellschaft'>
+                </a>
             </div>
             <div class="project-description">
                 <h3>CRIS I Visualization</h3>
